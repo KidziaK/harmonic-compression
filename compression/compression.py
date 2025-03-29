3833e60f9a86cbf18535d356519ef57990d8d986
Mikolaj Kida <mikolaj.kida@skyengine.ai>
2025-03-29 15:26:37 +0100

    feat: first working version :D

--- compression/compression.py
+++ compression/compression.py
@@ -9,10 +9,10 @@ import numba_scipy
 
 from utility.e57 import load_e57
 
-l_max = 10
+l_max = 3
 wigner = spherical.Wigner(l_max + 1) # We need +1 for recurrence relation for derivative of wigner d
 
-@njit(parallel=True, cache=True)
+@njit(parallel=True)
 def cartesian_to_spherical(points):
     """
     Given a numpy array of 3D points in Cartesian coordinates, convert them to spherical coordinates.
@@ -38,7 +38,7 @@ def cartesian_to_spherical(points):
     return result
 
 
-@njit(parallel=True, cache=True)
+@njit(parallel=True)
 def spherical_to_cartesian(r, theta, phi):
     """
     Given a numpy array of 3D points in spherical coordinates, convert them to Cartesian coordinates.
@@ -58,7 +58,7 @@ def spherical_to_cartesian(r, theta, phi):
         result[i] = np.array([x, y, z])
     return result
 
-@njit(parallel=True, cache=True)
+@njit
 def B(l: int):
     return np.sqrt((2 * l + 1) / 4 / np.pi)
 
@@ -69,7 +69,15 @@ def g(l, kappa):
     """
     return (B(l) * iv(l + 0.5, kappa) / iv(0.5, kappa)).astype(np.complex128)
 
-@njit(parallel=True, cache=True)
+@njit
+def g_(l, kappa):
+    """
+    Harmonic coefficient of order l of standard (mu is z-axis aligned) 3D von-mises-fisher distribution.
+    Since mu = (0, 0, 1), all complex spherical harmonic coefficients are equal to 0.
+    """
+    return B(l) * iv(l + 0.5, kappa) / iv(0.5, kappa)
+
+@njit(parallel=True)
 def harmonic_f(wigner_D, f_tilda, ell_max: int):
     num_points = wigner_D.shape[0]
     f = np.zeros(shape=(num_points, ell_max + 1, ell_max + 1), dtype=np.complex128)
@@ -80,9 +88,9 @@ def harmonic_f(wigner_D, f_tilda, ell_max: int):
     return f
 
 def compress(spherical_coords):
-    kappa = spherical_coords[:, 0]
-    theta = spherical_coords[:, 1]
-    phi = spherical_coords[:, 2]
+    kappa = spherical_coords[:, 0].astype(np.float64)
+    theta = spherical_coords[:, 1].astype(np.float64)
+    phi = spherical_coords[:, 2].astype(np.float64)
 
     l = np.arange(0, l_max + 1)
 
@@ -96,11 +104,11 @@ def compress(spherical_coords):
 
     return harmonic_f(D, f_tilda, l_max)
 
-@njit(parallel=True, cache=True)
+@njit(parallel=True)
 def N(l: int):
     return np.sqrt((2 * l + 1) / (4 * np.pi))
 
-@njit(parallel=True, cache=True)
+@njit(parallel=True)
 def dg_dkappa(kappa):
     n = len(kappa)
     dg = np.zeros(shape=(n, l_max + 1), dtype=np.complex128)
@@ -117,109 +125,169 @@ def dg_dkappa(kappa):
             dg[i, l] = first_term + second_term - third_term
     return dg
 
-@njit(parallel=True, cache=True)
+@njit(parallel=True)
 def df_dkappa(wigner_D, kappa, ell_max: int):
     num_points = wigner_D.shape[0]
     df = np.zeros(shape=(num_points, ell_max + 1, ell_max + 1), dtype=np.complex128)
+    sqrt_2pi = np.sqrt(2 * np.pi)
     for i in prange(num_points):
+        k = kappa[i]
+        sqrt_k = np.sqrt(k)
+        csch_k = 1 / np.sinh(k)
+        coth_k = np.cosh(k) / np.sinh(k)
         for l in range(ell_max + 1):
+            first_term = csch_k * iv(k, l + 0.5) / (4 * sqrt_2pi * sqrt_k)
+            second_term = sqrt_k * csch_k * (iv(k, l - 0.5) + iv(k, l + 1.5)) / (4 * sqrt_2pi)
+            third_term = sqrt_k * coth_k * csch_k * iv(k, l + 0.5) / (2 * sqrt_2pi)
+            dg = first_term + second_term - third_term
             for m in range(l + 1):
-                df[i, l, m] = wigner_D[i, spherical.WignerDindex(l, 0, m, 0, ell_max)] * dg_dkappa(kappa)
+                df[i, l, m] = wigner_D[i, spherical.WignerDindex(l, 0, m, 0, ell_max)] * dg
     return df
 
-@njit(parallel=True, cache=True)
-def df_dtheta(kappa, theta, phi, ell_max: int):
-    num_points = d.shape[0]
+@njit
+def factorial(x):
+    if x == 0:
+        return 1
+    return x * factorial(x - 1)
+
+@njit(parallel=True)
+def df_dtheta(d, kappa, theta, phi, ell_max: int):
+    num_points = len(theta)
     df = np.zeros(shape=(num_points, ell_max + 1, ell_max + 1), dtype=np.complex128)
 
     for i in prange(num_points):
-        d = wigner.d(np.exp(1j * theta[i]))
         for l in range(ell_max + 1):
             for m in range(l + 1):
                 if m == 0:
-                    dd = -np.sqrt(l * (l+1)) * d[spherical.WignerDindex(l, 0, m + 1, 0, ell_max)]
+                    dd = -np.sqrt(l * (l+1)) * d[i, spherical.WignerDindex(l, 0, m + 1, 0, ell_max)]
                     dD = np.exp(-1j * m * phi[i]) * dd
-                    df[i, l, m] = dD * g(l, kappa[i])
+                    df[i, l, m] = dD * g_(l, kappa[i])
                 else:
-                    constant = 0.5 * np.sqrt(math.factorial(l - m)/math.factorial(l + m))
-                    legendre_1 = (l + m) * (l - m + 1) * lpmv(m - 1, l, theta[i])
-                    legendre_2 = lpmv(m + 1, l, theta[i])
+                    constant = 0.5 * np.sqrt(factorial(l - m)/factorial(l + m))
+                    legendre_1 = (l + m) * (l - m + 1) * lpmv(float(m - 1), float(l), np.cos(theta[i]))
+                    legendre_2 = lpmv(float(m - 1), float(l), np.cos(theta[i]))
                     dD = np.exp(-1j * m * phi[i]) * constant * (legendre_1 - legendre_2)
-                    df[i, l, m] = dD * g(l, kappa[i])
+                    df[i, l, m] = dD * g_(l, kappa[i])
 
     return df
 
-@njit(parallel=True, cache=True)
-def df_dphi(theta, phi, ell_max: int):
-    num_points = d.shape[0]
+@njit(parallel=True)
+def df_dphi(d, kappa, theta, phi, ell_max: int):
+    num_points = len(theta)
     df = np.zeros(shape=(num_points, ell_max + 1, ell_max + 1), dtype=np.complex128)
 
     for i in prange(num_points):
-        d = wigner.d(np.exp(1j * theta[i]))
         for l in range(ell_max + 1):
             for m in range(l + 1):
-                dml = d[spherical.WignerDindex(l, 0, m, 0, ell_max)]
-                df[i, l, m] = - 1j * m * np.exp(-1j * m * phi[i]) * dml
+                dml = d[i, spherical.WignerDindex(l, 0, m, 0, ell_max)]
+                df[i, l, m] = - 1j * m * np.exp(-1j * m * phi[i]) * dml * g_(l, kappa[i])
 
     return df
 
+step = 0
+
 def loss(spherical_coords_flat, true_coefficients):
     n = len(spherical_coords_flat)
+    global step
     spherical_coords = spherical_coords_flat.reshape((3, n // 3)).T
     predicted_coefficients = compress(spherical_coords)
     F_tilda = predicted_coefficients.sum(axis=0)
     F = true_coefficients.sum(axis=0)
     diff = F_tilda - F
-    return np.real(np.conjugate(diff) * diff).sum()
-
-def grad_loss(spherical_coords_flat, true_coefficients):
-    n = len(spherical_coords_flat)
-    spherical_coords = spherical_coords_flat.reshape((3, n // 3)).T
-    predicted_coefficients = compress(spherical_coords)
-    F_tilda = predicted_coefficients.sum(axis=0)
-    F = true_coefficients.sum(axis=0)
-    diff = F_tilda - F
-
-    kappa = spherical_coords[:, 0]
-    theta = spherical_coords[:, 1]
-    phi = spherical_coords[:, 2]
-
-    q = quaternionic.array.from_euler_angles(theta, phi, np.zeros_like(theta))
-
-    D = wigner.D(q)
-
-    dkappa = df_dkappa(D, kappa, l_max)
-    dtheta = df_dtheta(kappa, theta, phi, l_max)
-    dphi = df_dphi(theta, phi, l_max)
-
-    dL_dkappa = 2 * np.real(np.conjugate(dkappa) * diff).sum(axis=(1, 2))
-    dL_dtheta = 2 * np.real(np.conjugate(dtheta) * diff).sum(axis=(1, 2))
-    dL_dphi = 2 * np.real(np.conjugate(dphi) * diff).sum(axis=(1, 2))
-
-    return np.array([dL_dkappa, dL_dtheta, dL_dphi])
+    L = np.real(np.conjugate(diff) * diff).sum()
+    # print(f"Loss at step {step} = {L}")
+    step += 1
+    return L
+#
+# def grad_loss(spherical_coords_flat, true_coefficients):
+#     n = len(spherical_coords_flat)
+#     spherical_coords = spherical_coords_flat.reshape((3, n // 3)).T
+#     predicted_coefficients = compress(spherical_coords)
+#     F_tilda = predicted_coefficients.sum(axis=0)
+#     F = true_coefficients.sum(axis=0)
+#     diff = F_tilda - F
+#
+#     kappa = spherical_coords[:, 0].astype(np.float64)
+#     theta = spherical_coords[:, 1].astype(np.float64)
+#     phi = spherical_coords[:, 2].astype(np.float64)
+#
+#     q = quaternionic.array.from_euler_angles(theta, phi, np.zeros_like(theta))
+#
+#     D = wigner.D(q)
+#     d_list = []
+#     for i in range(len(theta)):
+#         d_list.append(wigner.d(np.exp(1j * theta[i])))
+#     d = np.array(d_list)
+#
+#     dkappa = df_dkappa(D, kappa, l_max)
+#     dtheta = df_dtheta(d, kappa, theta, phi, l_max)
+#     dphi = df_dphi(d, kappa, theta, phi, l_max)
+#
+#     dL_dkappa = np.real(np.conjugate(dkappa) * diff).mean(axis=(1, 2))
+#     dL_dtheta = np.real(np.conjugate(dtheta) * diff).mean(axis=(1, 2))
+#     dL_dphi = np.real(np.conjugate(dphi) * diff).mean(axis=(1, 2))
+#
+#     return -np.hstack([dL_dkappa, dL_dtheta, dL_dphi])
 
 if __name__ == "__main__":
     import time
 
-    points = load_e57("../data/cube.e57")
+    points = load_e57("../data/Bunny.e57")[100:150]
+    # points = load_e57("../data/cube.e57")
 
-    radius = np.linalg.norm(points, axis=1, keepdims=True)
-    radius_scaled = (np.arctan(radius) / np.pi + 0.5) * 40 + 10
-
-    points = points / radius * radius_scaled
-
+    radius_max = np.linalg.norm(points, axis=1, keepdims=True).max()
+    points = points / radius_max
     spherical_coords = cartesian_to_spherical(points)
 
-    kappa_start = np.ones_like(spherical_coords[:, 0])
-    theta_start = spherical_coords[:, 1]
-    phi_start = spherical_coords[:, 2]
+    kappa = spherical_coords[:, 0].astype(np.float64)
+    theta = spherical_coords[:, 1].astype(np.float64)
+    phi = spherical_coords[:, 2].astype(np.float64)
+
+    error_theta = 0.1
+
+    kappa_start = 0.5 * np.ones_like(spherical_coords[:, 0])
+    theta_start = np.clip(spherical_coords[:, 1] + np.random.uniform(-error_theta, error_theta), 0.0, 2*np.pi)
+    phi_start = np.clip(spherical_coords[:, 2] + np.random.uniform(-0.1, 0.1), 0.0, np.pi)
+    # theta_start = np.random.uniform(0, 2 * np.pi, size=len(theta))
+    # phi_start = np.random.uniform(0, np.pi, size=len(phi))
 
     st = time.time()
-    harmonic_coefficients = compress(spherical_coords)
+    harmonic_coefficients = compress(spherical_coords).astype(np.complex64)
     end = time.time()
 
     print(f"compression took: {end - st} seconds")
 
-    print(harmonic_coefficients)
+    n = len(kappa_start)
+    bounds_kappa = np.array([[0, 1] for _ in range(n)])
+    bounds_theta = np.array([[theta[i] - error_theta, theta[i] + error_theta] for i in range(n)])
+    bounds_theta =  np.clip(bounds_theta, 0, 2 * np.pi)
 
-    reconstruction = scipy.optimize.minimize(loss, np.hstack([kappa_start, theta_start, phi_start]), harmonic_coefficients, jac=grad_loss)
+    bounds_phi = np.array([[0, np.pi] for _ in range(n)])
+
+    bounds = np.concatenate([bounds_kappa, bounds_theta, bounds_phi])
+
+    reconstruction = scipy.optimize.minimize(loss, np.hstack([kappa_start, theta_start, phi_start]), harmonic_coefficients, bounds=bounds
+                                             # , jac=grad_loss
+                                             )
+    kappa_reconstructed = reconstruction.x[:n]
+    theta_reconstructed = reconstruction.x[n:2*n]
+    phi_reconstructed = reconstruction.x[2*n:3*n]
+
+    from sklearn.metrics import mean_squared_error
+
+    print(f"MSE(kappa) = {mean_squared_error(kappa, kappa_reconstructed)}")
+    print(f"MSE(theta) = {mean_squared_error(theta, theta_reconstructed)}")
+    print(f"MSE(phi) = {mean_squared_error(phi, phi_reconstructed)}")
+
+    print(f"PSNR(kappa) = {-10 * math.log(mean_squared_error(kappa, kappa_reconstructed))}")
+    print(f"PSNR(theta) = {20*np.log10(2 * np.pi) -10 * math.log(mean_squared_error(theta, theta_reconstructed))}")
+    print(f"PSNR(phi) = {20*np.log10(2 * np.pi)-10 * math.log(mean_squared_error(phi, phi_reconstructed))}")
+
+    st = time.time()
+    reconstructed_points = spherical_to_cartesian(kappa, theta, phi)
+    end = time.time()
+
+    print(f"reconstruction took: {end - st} seconds")
+
+    # print(points)
+    # print(reconstructed_points)
